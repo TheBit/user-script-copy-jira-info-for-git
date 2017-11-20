bdbe4a69734c1cca20d8059ff0c6c0271b814293
Andriy Halyasovskyy <thebit.hao@gmail.com>
2017-11-20 12:40:42 +0200

    Further enhancing replaces

--- src/Copy_JIRA_info_for_Git.user.js
+++ src/Copy_JIRA_info_for_Git.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copy JIRA info for Git
 // @namespace    https://github.com/TheBit/user-script-copy-jira-info-for-git
-// @version      1.3
+// @version      1.4
 // @description  try to take over the world!
 // @author       TheBit
 // @license MIT
@@ -42,13 +42,16 @@
     }
 
     let grabbedInfoForBranch = `${$('#summary-val').innerText}`.
-        replace(/\s\s|\[.*?\]/ig, '')./* Strip double spaces and tags in square brackets e.g.: [tag] */
+        replace(/\s\s/ig, ' ')./* Replace double spaces with single space */
+        replace(/\[.*?\]/ig, '')./* Strip tags in square brackets e.g.: [tag] */
         trim()./* After removing square brackets - there might be leading white space left, so need to trim */
         replace(/\s/ig, '-'). /* Replace all spaces with dashes */
         replace(/^[\./]|\.\.|@{|[\/\.]$|^@$|[~^:\x00-\x20\x7F\s?*[\]\\]/ig, '')./* Strip all forbidden chars */
         split('-').slice(0, 5).join('-'); /* Take only first 5 words */
 
-    let grabbedInfoForCommit = `[${$('#key-val').innerText}]%tags% ${$('#summary-val').innerText}`;
+    let grabbedInfoForCommit = `[${$('#key-val').innerText}]%tags% ${$('#summary-val').innerText}`.
+        replace(/\s\s/ig, ' ')./* Replace double spaces with single space */
+        replace(/\[.*?\]/ig, '');/* Strip tags in square brackets e.g.: [tag] */
 
     const branchButtonNames = ['feature', 'bugfix', 'hotfix'];
     const branchButtons = branchButtonNames.map(name => createBranchButton(name, `${$('#key-val').innerText}-${grabbedInfoForBranch}`));
